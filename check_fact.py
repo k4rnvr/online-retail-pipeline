d68cd55019194083ba2045ada3333b9735d6f948
k4rnvr <veerkaransingh2612@gmail.com>
2025-09-17 18:57:30 +0530

    Remove troubleshooting scripts from repo

--- check_fact.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("staging/fact_sales.csv")
-print(df.columns.tolist())
--- test1.py
+++
@@ -1,9 +0,0 @@
-import os
-
-base_dir = os.path.dirname(os.path.abspath(__file__))
-sql_path = os.path.join(base_dir, "..", "queries", "distinct_countries.sql")
-print("Looking for SQL file at:", os.path.abspath(sql_path))
-print("Exists?", os.path.exists(sql_path))
-print("Files in folder:", os.listdir(os.path.join(base_dir, "..", "queries")))
-
-#/Users/karanveersingh/online_retail_pipeline/queries/distinct_countries.sql
